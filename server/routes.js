c43022732fc6c898459ab81ced7d68735bf9483c
Michal Ostruszka <michal.ostruszka@gmail.com>
2015-07-17 23:48:13 +0200

    Add basic Auth token presence check

--- server/routes.js
+++ server/routes.js
@@ -2,12 +2,23 @@ import {graphql} from 'graphql';
 
 import todoSchema from './graphql/schema';
 
+function ensureAuthTokenPresent(req, res, next) {
+	const authToken = req.get('Auth');
+	if(authToken && authToken.length) {
+		req.authToken = authToken;
+		return next();
+	}
+	return res.status(403).json({ error: 'Auth token not found in request' });
+}
+
 function handlePost(req, res) {
-  graphql(todoSchema, req.body).then((result) => {
+	const context = { authToken: req.authToken }
+	const query = req.body;
+  graphql(todoSchema, query, context).then((result) => {
     res.send(result);
   });
 }
 
 export default (app) => {
-  app.post('/api', handlePost)
+  app.post('/api', ensureAuthTokenPresent, handlePost)
 };
